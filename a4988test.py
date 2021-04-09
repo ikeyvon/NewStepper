fbc9894e8f53c4c0408283fa06f985e87c3d928c
nbeveridge <neil.h.beveridge@gmail.com>
2021-04-09 04:54:36 +0100

    .

--- a4988test.py
+++ a4988test.py
@@ -60,18 +60,20 @@ GPIO.setup(BACKWARD_BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 motor.motor_go(clockwise=False, steptype="Full", steps=250, stepdelay=.005, verbose=False, initdelay=.05)
 
+pressure = 1
+
 while True:
-    print('HERE')
-    pressure = sensor.read_sensor_test()
-    
+#     print('HERE')
+#     pressure = sensor.read_sensor_test()
+#     
     if GPIO.input(FORWARD_BUTTON_GPIO) == True:
         print(1234567890)
 
     if GPIO.input(FORWARD_BUTTON_GPIO) == True  and pressure<=high_pressure_threshold:
         print('for')
-        motor.motor_go(clockwise=False, steptype="Full", steps=50, stepdelay=.005, verbose=False, initdelay=.05)
+        motor.motor_go(clockwise=False, steptype="Full", steps=100, stepdelay=.005, verbose=False, initdelay=.05)
         
 
     elif GPIO.input(BACKWARD_BUTTON_GPIO) == True  and pressure>=low_pressure_threshold:
-        motor.motor_go(clockwise=True, steptype="Full", steps=50, stepdelay=.005, verbose=False, initdelay=.05)
+        motor.motor_go(clockwise=True, steptype="Full", steps=100, stepdelay=.005, verbose=False, initdelay=.05)
         print('back')
